498801b2d31bf988a8d2cfd580c3619ff820559f
Shahil00099 <your-github-shahilshrestha99@gmail.com>
2026-08-20 12:35:44 +0545

    Import Tailwind CSS for production styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- THIS LINE LOADS TAILWIND STYLES
+import './index.css' // <-- Make sure this line is present
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
