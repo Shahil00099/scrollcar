28381e2d04cd469e6325afa0c03e190faeae2575
Shahil00099 <your-github-shahilshrestha99@gmail.com>
2026-08-26 14:11:00 +0545

    Include Tailwind plugin and CSS imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- Make sure this line exists!
+import './index.css' // <-- Critical: loads Tailwind and App.css styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
